880f570a14487b478c52d457a2c76301481d429b
Mihifidem <mihifidem@gmail.com>
2025-10-24 11:40:04 +0200

    parte A

--- core/permissions.py
+++ core/permissions.py
@@ -1,4 +1,4 @@
 from django.core.exceptions import PermissionDenied
 
 def is_admin(user):
-    return user.is_authenticated and getattr(getattr(user,"profile",None),"role",None)=="admin"
+    return user.is_authenticated and getattr(getattr(user,"profileuser",None),"role",None)=="admin"
